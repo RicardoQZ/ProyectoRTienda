0ccde54a674e0ecb0b30cc07adfe64ad96b891fc
RicardoQZ <ricardopavelquispez@gmail.com>
2024-12-10 23:04:20 -0500

    Obteniendo Productos desde appwrite

--- src/pages/Productos.tsx
+++ src/pages/Productos.tsx
@@ -2,11 +2,14 @@ import { useEffect, useState } from "react"
 import useFetch from "../shared/hooks/useFetch"
 import { StoreEndpoints, StoreProductos } from "../shared/declarations/FakeStore"
 import Producto from "../shared/components/Producto"
-import { Box} from "@chakra-ui/react"
+import { Box, Image, Text} from "@chakra-ui/react"
+import { database } from "../shared/lib/appwrite"
+import { Appwrite } from "../shared/lib/env"
 
 const Productos = () => {
   const [productos,setProductos]=useState<Array<StoreProductos>>()
-  
+  const [appwriteProducts, setAppwriteProducts] = useState<Array<object>>()
+
   const {get} =useFetch(StoreEndpoints.PRODUCTOS)
 
   const getProductos=async()=>{
@@ -15,13 +18,37 @@ const Productos = () => {
     setProductos(productos)
   }
 
+  const getProductosAppwrite=async()=>{
+    const { documents }= await database.listDocuments(Appwrite.databaseId,Appwrite.collections.products)
+
+    setAppwriteProducts(documents)
+  }
+
   useEffect(()=>{
     getProductos()
+    getProductosAppwrite()
   },[])
 
   return (
     <>
+       <Box w='65%' m='0 auto'>
+        <h3>Mis Productos</h3>
+        <hr />
+        {
+            appwriteProducts?.map(product => (
+                <Box key={product.nombre_prod}>
+                    <Image src={product.linkphoto} w='100px'></Image>
+                    <Text>{product.nombre_prod}</Text>
+                    <Text>{product.precio}</Text>
+                    <Text>{product.descripcion}</Text>
+                </Box>
+            ))
+        }
+      </Box>
+      <br />
+      <br />
       <Box display='flex' flexWrap='wrap' w='65%' m='0 auto' justifyContent='space-between' gap='1em'>
+     
         {
           productos && productos.map(p=>(
             <Producto key={p.id} producto={p}/>
--- src/shared/lib/appwrite.ts
+++ src/shared/lib/appwrite.ts
@@ -1,5 +1,12 @@
-import { Client } from 'appwrite'
-import { AppProject } from './env'
+import { Client, Databases } from 'appwrite'
+import { Appwrite } from './env'
 
 const client = new Client()
-client.setProject(AppProject.projectId)
+client.setProject(Appwrite.projectId)
+    .setEndpoint('https://cloud.appwrite.io/v1')
+
+const database = new Databases(client)
+
+export {
+    database
+}
